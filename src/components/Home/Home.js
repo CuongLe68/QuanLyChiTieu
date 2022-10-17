541aff4415e4829d901884bcc25ef3d4bbe74835
CuongLe68 <cuongle.23012001@gmail.com>
2022-10-18 05:59:10 +0700

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,91 +21,96 @@ function Home() {
   };
 
   return (
-    <div className={cx("wrapper")}>
-      {/* form login */}
-      <div id="form-login" className={cx("wrapper-form")}>
-        <div className={cx("form-login")}>
-          <h1 className={cx("form-title")}>ĐĂNG NHẬP</h1>
-          <div className={cx("form-col")}>
-            <div className={cx("form-col-title")}>Tài khoản</div>
-            <input className={cx("form-col-input")} type="text" />
-          </div>
-          <div className={cx("form-col")}>
-            <div className={cx("form-col-title")}>Mật khẩu</div>
-            <input className={cx("form-col-input")} type="password" />
-          </div>
-        </div>
-        <button className={cx("form-btn form-btn-login")}>Đăng nhập</button>
-        <button
-          className={cx("form-btn form-btn-back")}
-          onClick={handleRemoveLogin}
-        >
-          Quay lại
-        </button>
+    <>
+      <div className={cx("error")}>
+        Chỉ sử dụng cho điện thoại có kích thước nhỏ hơn 414px
       </div>
-
-      {/* form Register */}
-      <div id="form-register" className={cx("wrapper-form")}>
-        <div className={cx("form-register")}>
-          <h1 className={cx("form-title")}>ĐĂNG KÝ</h1>
-          <div className={cx("form-col")}>
-            <div className={cx("form-col-title")}>Tài khoản</div>
-            <input className={cx("form-col-input")} type="text" />
+      <div className={cx("wrapper")}>
+        {/* form login */}
+        <div id="form-login" className={cx("wrapper-form")}>
+          <div className={cx("form-login")}>
+            <h1 className={cx("form-title")}>ĐĂNG NHẬP</h1>
+            <div className={cx("form-col")}>
+              <div className={cx("form-col-title")}>Tài khoản</div>
+              <input className={cx("form-col-input")} type="text" />
+            </div>
+            <div className={cx("form-col")}>
+              <div className={cx("form-col-title")}>Mật khẩu</div>
+              <input className={cx("form-col-input")} type="password" />
+            </div>
           </div>
-          <div className={cx("form-col")}>
-            <div className={cx("form-col-title")}>Mật khẩu</div>
-            <input className={cx("form-col-input")} type="password" />
-          </div>
-          <div className={cx("form-col")}>
-            <div className={cx("form-col-title")}>Email</div>
-            <input className={cx("form-col-input")} type="email" />
-          </div>
-          <div className={cx("form-col")}>
-            <div className={cx("form-col-title")}>Họ tên</div>
-            <input className={cx("form-col-input")} type="text" />
-          </div>
+          <button className={cx("form-btn form-btn-login")}>Đăng nhập</button>
+          <button
+            className={cx("form-btn form-btn-back")}
+            onClick={handleRemoveLogin}
+          >
+            Quay lại
+          </button>
         </div>
-        <button className={cx("form-btn form-register-btn-register")}>
+
+        {/* form Register */}
+        <div id="form-register" className={cx("wrapper-form")}>
+          <div className={cx("form-register")}>
+            <h1 className={cx("form-title")}>ĐĂNG KÝ</h1>
+            <div className={cx("form-col")}>
+              <div className={cx("form-col-title")}>Tài khoản</div>
+              <input className={cx("form-col-input")} type="text" />
+            </div>
+            <div className={cx("form-col")}>
+              <div className={cx("form-col-title")}>Mật khẩu</div>
+              <input className={cx("form-col-input")} type="password" />
+            </div>
+            <div className={cx("form-col")}>
+              <div className={cx("form-col-title")}>Email</div>
+              <input className={cx("form-col-input")} type="email" />
+            </div>
+            <div className={cx("form-col")}>
+              <div className={cx("form-col-title")}>Họ tên</div>
+              <input className={cx("form-col-input")} type="text" />
+            </div>
+          </div>
+          <button className={cx("form-btn form-register-btn-register")}>
+            Đăng ký
+          </button>
+          <button
+            className={cx("form-btn form-register-btn-back")}
+            onClick={handleRemoveRegister}
+          >
+            Quay lại
+          </button>
+        </div>
+
+        {/* home */}
+        <FontAwesomeIcon icon={faBars} className={cx("menu-icon")} />
+        <FontAwesomeIcon
+          icon={faHeart}
+          className={cx("menu-icon-heart menu-icon-heart-1")}
+        />
+        <FontAwesomeIcon
+          icon={faHeart}
+          className={cx("menu-icon-heart menu-icon-heart-2")}
+        />
+        <FontAwesomeIcon
+          icon={faHeart}
+          className={cx("menu-icon-heart menu-icon-heart-3")}
+        />
+        <FontAwesomeIcon
+          icon={faHeart}
+          className={cx("menu-icon-heart menu-icon-heart-4")}
+        />
+        <FontAwesomeIcon
+          icon={faHeart}
+          className={cx("menu-icon-heart menu-icon-heart-5")}
+        />
+        <div className={cx("title")}>Gia vị tình yêu</div>
+        <div className={cx("btn-login")} onClick={handleShowLogin}>
+          Đăng nhập
+        </div>
+        <div className={cx("btn-register")} onClick={handleShowRegister}>
           Đăng ký
-        </button>
-        <button
-          className={cx("form-btn form-register-btn-back")}
-          onClick={handleRemoveRegister}
-        >
-          Quay lại
-        </button>
+        </div>
       </div>
-
-      {/* home */}
-      <FontAwesomeIcon icon={faBars} className={cx("menu-icon")} />
-      <FontAwesomeIcon
-        icon={faHeart}
-        className={cx("menu-icon-heart menu-icon-heart-1")}
-      />
-      <FontAwesomeIcon
-        icon={faHeart}
-        className={cx("menu-icon-heart menu-icon-heart-2")}
-      />
-      <FontAwesomeIcon
-        icon={faHeart}
-        className={cx("menu-icon-heart menu-icon-heart-3")}
-      />
-      <FontAwesomeIcon
-        icon={faHeart}
-        className={cx("menu-icon-heart menu-icon-heart-4")}
-      />
-      <FontAwesomeIcon
-        icon={faHeart}
-        className={cx("menu-icon-heart menu-icon-heart-5")}
-      />
-      <div className={cx("title")}>Gia vị tình yêu</div>
-      <div className={cx("btn-login")} onClick={handleShowLogin}>
-        Đăng nhập
-      </div>
-      <div className={cx("btn-register")} onClick={handleShowRegister}>
-        Đăng ký
-      </div>
-    </div>
+    </>
   );
 }
 
